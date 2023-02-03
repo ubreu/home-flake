6e28e369cdc9a0dbfcf5451541cbfd12ff49fb76
Urs Breu <dev@gleisdrei.ch>
2023-02-03 17:12:49 +0100

    use flake-utils and generate packages for all supported systems

--- flake.nix
+++ flake.nix
@@ -4,17 +4,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager }: 
-  let 
-    system = "x86_64-darwin"; 
-
-    ubreu = home-manager.lib.homeManagerConfiguration {  
-        pkgs = nixpkgs.legacyPackages.${system};
-        modules = [ ./home.nix ];
-    };
-  in {
-    defaultPackage.${system} = ubreu.activationPackage;
-  };
+  outputs = { self, nixpkgs, home-manager, flake-utils }: 
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      ubreu = home-manager.lib.homeManagerConfiguration {  
+          pkgs = nixpkgs.legacyPackages.${system};
+          modules = [ ./home.nix ];
+      };
+    in {
+      packages.default = ubreu.activationPackage;
+    });
 }
