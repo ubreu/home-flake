98b6af472e7266fb6ea4ef067c5abeff36dd1966
Urs Breu <urs.breu@ergon.ch>
2023-02-06 08:52:58 +0100

    remove nvm, fix alias for visual studio code, add homebrew shellenv

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, flake-utils }: 
